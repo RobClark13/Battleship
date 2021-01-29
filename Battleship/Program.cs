95031de2e0a6390ab72f62921eae4a386092bf35
Rob Clark <clarkrob13@gmail.com>
2021-01-29 11:00:29 -0500

    Initial Classes Created

---
+++ Battleship/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr2d = new int[20, 20];
+            for (int y = 0; y < 20; y++)
+            {
+                for (int x = 0; x < 20; x++)
+                {
+                    arr2d[x, y] = x+y;
+                }
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < 20; j++)
+                {
+                    Console.Write("{0}\t", arr2d[i,j]);
+                }
+            }
+           
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Battleship/Ships/AircraftCarrier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class AircraftCarrier:Ship
+    {
+        //member variables
+        int shipLength = 5;
+        
+        //constructor
+
+        //member methods
+    }
+}
